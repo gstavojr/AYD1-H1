9cb50097c4ebad5c7d5d98c4644923292242eb04
Gustavo Sanchez <3004395470101@ingenieria.usac.edu.gt>
2022-02-06 23:46:48 -0600

    Feature Backend
    Se creo los archivos base para el servidor

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+// Crear el servidor de express
+const app = express();
+
+// Puerto
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+// Rutas
+app.get('/', (req, res) =>
+  res.send({
+    curso: 'Analisis y Diseño 1',
+    nombre: 'Elmer Gustavo Sanchez Garcia',
+    carnet: 201801351,
+    aux: 'Manuel de Mata',
+  })
+);
+
+export default app;
